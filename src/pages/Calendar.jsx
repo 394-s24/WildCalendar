9fc1fa421ba3b1722baf441daa4974ea3f60ef8f
hibakhatib <hibakhatib2025@u.northwestern.edu>
2024-03-29 14:51:35 -0500

    basic routing

---
+++ src/pages/Calendar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cal = () => {
+
+    
+  return (
+    <div>
+      <p>calendar page here</p>
+    </div>
+  );
+};
+
+export default Cal;
---
+++ src/pages/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoList = () => {
+
+    
+  return (
+    <div>
+
+      <p>to do page here</p>
+    </div>
+  );
+};
+
+export default TodoList;

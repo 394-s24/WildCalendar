76e207da98912cd90910aff974d75e29b1d0bfd5
hibakhatib <hibakhatib2025@u.northwestern.edu>
2024-04-02 22:58:36 -0500

    working nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const Home = () => {
   return (
     <div>
       homepage!
+      <Navbar/>
     </div>
   );
 };
--- src/Navigation/Navbar.jsx
+++ src/Navigation/Navbar.jsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import '../App.css'
+import React, {useState} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <center>
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span className="navbar-toggler-icon"></span>
-            </button>
+    const [isMenuOpen, setMenuOpen] = useState(false);
 
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">
-                            Home
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="calendar">
-                            Calendar
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="todo">
-                            Todo
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </center>
-    </nav>
+    const toggleMenu = () => {
+      setMenuOpen(!isMenuOpen);
+    };
+    
+    return (
+     
+
+        <main>
+
+        <div className = 'menu-container'>
+          <div className = 'menu-button-left'>
+          <button className = 'menu-button'>
+          <Link to="/">WildCalendar</Link>
+          </button>
+          </div>
+  
+          <div className='menu-button-right'>
+            <button onClick={toggleMenu} className="menu-button">
+              menu
+            </button>
+            {/* <svg width="20px" height="20px" viewBox="0 -2 20 20" xmlns="http://www.w3.org/2000/svg" fill="none"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="#0000FF" fillRule="evenodd" d="M19 4a1 1 0 01-1 1H2a1 1 0 010-2h16a1 1 0 011 1zm0 6a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 7a1 1 0 100-2H2a1 1 0 100 2h16z"></path> </g></svg>           */}
+  
+            {isMenuOpen && (
+              <div className='menu'>
+                <Link to="/calendar">calendar</Link>
+
+                <Link to="/todo">todo</Link>
+              </div>
+            )}
+          </div>
+        </div>
+      </main>
     );
 }
 

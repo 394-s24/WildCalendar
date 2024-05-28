cc7673dfd4db690068c17a63cd4b099fb89a9147
Alexis Robles <alexis.robles49@gmail.com>
2024-05-28 14:54:33 -0500

    TodoItem refactored to use new API

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { ref, update, push, remove } from '@firebase/database';
-import { database } from '../firebase';
 import { Dropdown, Space, Button } from 'antd';
+import { updateData, removeData } from '../utilities/firebase';
 
 export default function TodoItem({ id, date, time, description, eventName, completed, startEditing, setStartEditing }) {
   const [isCompleted, setCompleted] = useState(completed);
@@ -9,32 +8,29 @@ export default function TodoItem({ id, date, time, description, eventName, compl
   const [editedText, setEditedText] = useState(description);
   const [editedDate, setEditedDate] = useState(date);
   const [editedTime, setEditedTime] = useState(time);
-  const [newDescription, setNewDescription] = useState('');
-  const [newDate, setNewDate] = useState('');
-  const [newTime, setNewTime] = useState('');
 
-  useEffect (
-    () => {
-      startEditing === id ? setEditing(true) : <></>
-    }, [startEditing]
-  )
+  useEffect(() => {
+    if (startEditing === id) {
+      setEditing(true);
+    }
+  }, [startEditing, id]);
 
-  const toggleCompleted = () => {
+  const toggleCompleted = async () => {
     const updatedCompleted = !isCompleted;
     setCompleted(updatedCompleted);
-    update(ref(database, `todo/${eventName}/${id}`), { description, completed: updatedCompleted });
+    await updateData(`todo/${eventName}/${id}`, { description, completed: updatedCompleted });
   };
 
-  const deleteItem = () => {
+  const deleteItem = async () => {
     console.log("Deleting");
-    remove(ref(database, `todo/${eventName}/${id}`));
+    await removeData(`todo/${eventName}/${id}`);
   };
 
-  const updateItem = () => {
-    update(ref(database, `todo/${eventName}/${id}`), { description: editedText, date: editedDate, time: editedTime });
+  const updateItem = async () => {
+    await updateData(`todo/${eventName}/${id}`, { description: editedText, date: editedDate, time: editedTime });
     setEditing(!editing);
     setStartEditing("");
-  }
+  };
 
   const items = [
     {
@@ -52,10 +48,8 @@ export default function TodoItem({ id, date, time, description, eventName, compl
 
   return (
     <div className="border rounded-xl p-4 mb-4 flex flex-row justify-between">
-
       <div className='flex items-start gap-2'>
         <input type="checkbox" className="cursor-pointer w-5 h-5" checked={isCompleted} onChange={toggleCompleted} />
-
         {editing ? (
           <>
             <input type="text" value={editedText} onChange={(e) => setEditedText(e.target.value)} />
@@ -63,10 +57,6 @@ export default function TodoItem({ id, date, time, description, eventName, compl
             <input type="time" value={editedTime} onChange={(e) => setEditedTime(e.target.value)} />
           </>
         ) : (
-          // <label style={{ textDecoration: isCompleted ? 'line-through' : 'none' }}>
-          //   {editedText} - {editedDate} - {editedTime}
-          // </label>
-
           <div className='flex flex-col gap-1'>
             <p>{editedText}</p>
             <p className='text-gray-500'>{editedDate}</p>
@@ -76,7 +66,7 @@ export default function TodoItem({ id, date, time, description, eventName, compl
       </div>
 
       {editing ? (
-        <Button onClick={() => updateItem()}>
+        <Button onClick={updateItem}>
           Save
         </Button>
       ) : (

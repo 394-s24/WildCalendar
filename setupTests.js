0ac6c05a1fb7d2233d20600dfb12af174aed4742
Alexis Robles <alexis.robles49@gmail.com>
2024-06-05 23:00:05 -0500

    Alexis unit tests done

---
+++ setupTests.js
@@ -0,0 +1,14 @@
+// setupTests.js
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  }),
+});
--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: './setupTests.js'
   },
   resolve: {
     alias: {
